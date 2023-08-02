066073e837c27c5045931d6ceb090bdb280c3c73
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-08-02 19:59:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Jayanna:Jayanna2023@yash.tm1c2bd.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "UploadLinkToFileBot")
+DB_NAME = os.environ.get("DATABASE_NAME", "New_Divya_Spandana")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
