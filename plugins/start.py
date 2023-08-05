a51b5433190c665b274e2282dc9477a7f6b05ed2
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-08-05 12:36:46 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -65,7 +65,7 @@ async def start_command(client: Client, message: Message):
             return
 
 
-        files = None
+        files = ""
         temp_msg1 = await temp_msg.edit(f"<b>ಈ ಫೈಲ್ ಒಂದು ಗಂಟೆಯ ನಂತರ ಡಿಲೀಟ್ ಆಗುತ್ತದೆ, ಆದ್ದರಿಂದ ಈ ಫೈಲ್ ಅನ್ನು ಫಾರ್ವರ್ಡ್ ಅಥವಾ ಸೇವ್ ಮಾಡಿಕೊಳ್ಳಿ</b>")
         for msg in messages:
 
