153636baa9fee352e15191e3009805ad7ef63394
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-08-05 11:55:29 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,7 +88,6 @@ async def start_command(client: Client, message: Message):
                 #await files.delete()
             except FloodWait as e:
                 await asyncio.sleep(e.x)
-                global files
                 files = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
                 #f_name = await files.reply_text(f"<b>ಈ ಫೈಲ್ ಒಂದು ಗಂಟೆಯ ನಂತರ ಡಿಲೀಟ್ ಆಗುತ್ತದೆ, ಆದ್ದರಿಂದ ಈ ಫೈಲ್ ಅನ್ನು ಫಾರ್ವರ್ಡ್ ಅಥವಾ ಸೇವ್ ಮಾಡಿಕೊಳ್ಳಿ</b>", quote=True, reply_markup=reply_markup)
                 #await asyncio.sleep(3600)
