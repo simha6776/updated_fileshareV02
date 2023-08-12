d5bad1bba2157047787a26d619e3b0e3f8c8508f
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-08-12 12:05:49 +0530

    Create api.py

---
+++ plugins/api.py
@@ -0,0 +1,58 @@
+odd = {
+#ZeeKannada
+'Trinayani_':[];
+'Annapoorna_':[];
+'Kalyanamasthu_':[];
+'Vaidehi_Parinaya_':[];
+'Punarvivaha_':[];
+'Paaru_':[];
+'Bhoomige_Bandha_Bhagavantha_':[];
+'Puttakkana_Makkalu_':[];
+'Gattimela_':[];
+'Manemagalu_':[];
+'Sandhyaraaga_':[];
+'SeethaRaama_':[];
+'Amruthadhaare_':[];
+'Mahanayaka_Dr_B_R_Ambedkar_':[];
+'Shrirasthu_Shubhamasthu_':[];
+'Soubhagyavati_Bhava_':[];
+'Hitler_Kalyana_':[];
+'Sathya_':[];
+'Dance_Karnataka_Dance_Season_7_':[];
+'Bharjari_Bachelors_':[];
+'Couples_Kitchen_':[];
+'Chota_Champion_':[];
+
+#ColorsKannada
+'Pavada_Purasha_':[];
+'Olavina_Nildana_':[];
+'Kendasampige_':[];
+'Bhagyalakshmi_':[];
+'Ramachari_':[];
+'Lakshmi_Baramma_':[];
+'Geetha_':[];
+'Lakshana_':[];
+'Punyavathi_':[];
+'Tripura_Sundari_':[];
+'Antarapata_':[];
+'Gruhapravesha_':[];
+'Ganda_Hendthi_':[];
+'Shantham_Papam_':[];
+'Muddu_Bangara_':[];
+'Gunasundari_':[];
+'Family_Gangstars_':[];
+
+#StarSuvarna
+}
+
+even = {
+  'Trinayani_':[];
+  'Annapoorna_':[];
+  'Kalyanamasthu_':[];
+'Vaidehi_Parinaya_':[];
+'Punarvivaha_':[];
+'Paaru_':[];
+'Bhoomige_Bandha_Bhagavantha_':[];
+'Puttakkana_Makkalu_':[];
+'Gattimela_':[];
+}
