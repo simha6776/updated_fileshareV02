f3bc7b3455f0c26e4fc397a82589c06b9890366f
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-08-14 01:20:30 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -11,11 +11,11 @@ from helper_func import encode
 import requests
 import string
 import re
-from cbb import cb_handler
+from cbb import dateday
 from pyshorteners import Shortener
 import aiohttp
 
-datee = cb_handler(Bot, CallbackQuery, datetime, dateday)
+datee = dateday[-1]
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command(["date"]))
 async def date(bot, message):
     reply_markup = InlineKeyboardMarkup(
