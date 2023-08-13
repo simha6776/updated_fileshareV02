f445d93777da52fed3a205eb96c525a61603c5fa
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-08-14 03:36:01 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -15,7 +15,6 @@ from plugins.cbb import DATEDAY
 from pyshorteners import Shortener
 import aiohttp
 
-datee = str(DATEDAY[-1])
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command(["date"]))
 async def date(bot, message):
     reply_markup = InlineKeyboardMarkup(
@@ -85,7 +84,7 @@ async def channel_post(client: Client, message: Message):
         #await reply_text.edit(f"<b>Here is your link</b>\n\n{Tlink}\n\n<code>{Tlink}</code>", reply_markup=reply_markup, disable_web_page_preview = True)
         
         Slink = await get_short(SL_URL,SL_API,Tlink)
-        await e_pic.edit(FOMET.format(datee, Slink, Slink))
+        await e_pic.edit(FOMET.format(DATEDAY[-1], Slink, Slink))
      #   if not DISABLE_CHANNEL_BUTTON:
      #       await post_message.edit_reply_markup(reply_markup)
 
