57372305a3d2991383585735b7cb73699701bd84
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-08-14 02:41:12 +0530

    Update data.py

--- plugins/data.py
+++ plugins/data.py
@@ -1,29 +1,3 @@
-
-#ZeeKannada
-# 'Trinayani_':['https://akamaividz2.zee5.com/image/upload/resources/0-1-6z5407624/list/00000161944ebc2c47794c7993f238d0c2e65f6f.jpg',];
-# 'Annapoorna_':['https://akamaividz2.zee5.com/image/upload/resources/0-1-6z5407922/list/00000182f03d445f7dc84ef396767131661dff5c.jpg',];
-# 'Kalyanamasthu_':['https://akamaividz2.zee5.com/image/upload/resources/0-1-6z5407692/list/00000145b73e51475b1f434a80f22ca025b30b9c.jpg',];
-# 'Vaidehi_Parinaya_':['https://akamaividz2.zee5.com/image/upload/resources/0-1-6z5407870/list/0000001315dbb5e90e02446bbb0793c8cc59ef52.jpg',];
-# 'Punarvivaha_':['https://akamaividz2.zee5.com/image/upload/resources/0-1-6z5407657/list/00000031dfe0764e605e4f719546d591ece2ab14.jpg',];
-# 'Paaru_':['https://akamaividz2.zee5.com/image/upload/resources/0-1-6z5407739/list/000000513e2e04dc27be4f27a8dfb2aca3317554.jpg',];
-# 'Bhoomige_Bandha_Bhagavantha_':['https://akamaividz2.zee5.com/image/upload/resources/0-1-6z5407943/list/00000127ab0e78e223e3491cb0f7dfe4fb89dcd6.jpg',];
-# 'Puttakkana_Makkalu_':['https://akamaividz2.zee5.com/image/upload/resources/0-1-6z5407846/list/000002101d1c5d16f8964fd2835873b18d65c00f.jpg',];
-# 'Gattimela_':['https://akamaividz2.zee5.com/image/upload/resources/0-1-6z5407544/list/00000065bc1e33dd18bb44e09c42275abe57cd44.jpg',];
-# 'Manemagalu_':['https://akamaividz2.zee5.com/image/upload/resources/0-1-6z5407602/list/00000150423451e6856f46d9b136e5bfc0f3b06d.jpg',];
-# 'Sandhyaraaga_':['https://akamaividz2.zee5.com/image/upload/resources/0-1-6z5407569/list/000001479ea2347355e54780ad4e16a3f8407c85.jpg',];
-# 'SeethaRaama_':['https://akamaividz2.zee5.com/image/upload/resources/0-6-4z5359283/list/1170x658withlogo166d1d2e5df44be2892a295293392b5e.jpg',];
-# 'Amruthadhaare_':['https://akamaividz2.zee5.com/image/upload/resources/0-6-4z5364077/list/1170x658withlogo8769f31132de46d39a97d2fd524a30c0.jpg',];
-# 'Mahanayaka_Dr_B_R_Ambedkar_':['https://akamaividz2.zee5.com/image/upload/resources/0-1-6z5403078/list/00000181c4f38dfde0494f6ba25963be14984ad7.jpg',];
-# 'Shrirasthu_Shubhamasthu_':['https://akamaividz2.zee5.com/image/upload/resources/0-1-6z5407897/list/00000164b53a6ebbe3794dac854e5d73b72517b8.jpg',];
-# 'Soubhagyavati_Bhava_':['https://akamaividz2.zee5.com/image/upload/resources/0-6-4z5363757/list/1170x658withlogod3dbd18bddbe42e881e39aebeb68d66b.jpg',];
-# 'Hitler_Kalyana_':['https://akamaividz2.zee5.com/image/upload/resources/0-1-6z5407717/list/000001392edaa9522bee4002bfd7a142c86c30b6.jpg',];
-# 'Sathya_':['https://akamaividz2.zee5.com/image/upload/resources/0-1-6z5407516/list/0000011143160c0020a94c3298ccfd6f671c761f.jpg',];
-# 'Dance_Karnataka_Dance_Season_7_':['https://akamaividz2.zee5.com/image/upload/resources/0-1-6z5403328/list/000003350ba2f10b747f44afa9aaf81cb6e1649b.jpg',];
-# 'Bharjari_Bachelors_':['https://akamaividz2.zee5.com/image/upload/resources/0-6-4z5383494/list/1170x658withlogo258d7eec65a74332b73ca82adc6ceff1.jpg',];
-# 'Couples_Kitchen_':['https://akamaividz2.zee5.com/image/upload/resources/0-6-4z5399211/list/1170x658withlogo593b1298f717409081a30029e5eeb102.jpg',];
-# 'Chota_Champion_':['https://akamaividz2.zee5.com/image/upload/resources/0-6-4z5351454/list/1170x658withlogod8c4d3a2969645ff820d4539bbb8bd4a.jpg',];
-
-#ColorsKannada
 ODD = {
         'Trinayani_':['https://graph.org/file/1f2827a0980cc418ee6e1.jpg','moneycase.link','3353ec87137e166f8561b86284728bd93e1b8bdf',''],
         'Annapoorna_':['https://graph.org/file/baff21a920cc09f0ba89a.jpg','moneycase.link','e9872aa279e0a57e59cf4ef3d20bea8c3be0571d',''],
