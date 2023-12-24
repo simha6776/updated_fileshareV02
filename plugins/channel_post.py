df616020ee884334198f2240eefc139b64a7c1ac
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-12-24 17:15:24 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -26,9 +26,16 @@ async def date(bot, message):
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.text & ~filters.command(['start','users','broadcast','batch','genlink','stats']))
 async def channel_post(client: Client, message: Message):
-    dateexc = datetime.now().strftime("%d")
+    current_time = datetime.now()
     media = message.video or message.document
-    filname= re.split("S\d", media.file_name)[0] #[1][2]etc
+    # filname= media.file_name.split("S0")[0]#[1][2]etc
+    ############# FOR UTSAV BOT ##################
+    #filname = re.split("S\d", media.file_name)[0]#[1][2]etc
+    ############# FOR DS BOT ##################
+    filname = re.split(current_time.strftime("%B"), media.file_name)[0]#[1][2]etc
+    
+    #Eno= re.findall("S\d+E\d+\d", media.file_name)                   
+    
     botfsno= re.findall("S0.+E\d+\d", media.file_name)
     if int(dateexc) % 2 != 0:
         if filname in ODD.keys():
